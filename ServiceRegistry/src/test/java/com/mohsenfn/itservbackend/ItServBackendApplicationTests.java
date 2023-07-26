e1f86097448a3a0f3c322a368a87825703a22516
mohsenfennira <mohsen.fennira@esprit.tn>
2023-07-26 13:18:29 +0100

    add Config Service + Service Registry

---
+++ ServiceRegistry/src/test/java/com/mohsenfn/itservbackend/ItServBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohsenfn.itservbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItServBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
